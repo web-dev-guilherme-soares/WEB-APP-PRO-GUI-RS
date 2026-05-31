0835494dc1e43389f801502de6b1caa4dd248df0
web-dev-guilherme-soares <webdevguilhermes@gmail.com>
2026-05-31 13:25:06 -0300

    Delete WEB-APP-PRO-GUI_MAIN directory

--- WEB-APP-PRO-GUI_MAIN/5_INTRODUCTION/introduction.js
+++
@@ -1,7 +0,0 @@
-import * as gui_main from "../0_SETTINGS/gui_main.js";
-
-gui_main.func_add_onclick("m_img_close", function() { gui_main.func_back_window(); });
-
-gui_main.func_add_onclick("btn_ds", function() { gui_main.func_open_new_window("../GAMES/0_game.html"); });
-
-gui_main.m_show_dialog(false, "VOID-777", "VOID-777 - Haha, vamos estudar. No\nfinal, tem um desafio divertido. 😄");
