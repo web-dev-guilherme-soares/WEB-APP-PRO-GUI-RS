16a901963d511d8e24758924f7644207527f120f
web-dev-guilherme-soares <webdevguilhermes@gmail.com>
2026-05-31 18:24:34 -0300

    Delete WEB-APP-PRO-GUI_MAIN directory

--- WEB-APP-PRO-GUI_MAIN/3_PROFILE/profile.js
+++
@@ -1,5 +0,0 @@
-import * as gui_main from "../0_SETTINGS/gui_main.js";
-
-gui_main.func_add_onclick("m_img_close", function() { gui_main.func_back_window(); });
-
-gui_main.m_show_dialog(false, "VOID-777", "VOID-777 - Ops... parece que você\nencontrou o perfil do desenvolvedor 😯");
