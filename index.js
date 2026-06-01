4bc41fbc16c9a2fa6086e218dc3480edd892edda
web-dev-guilherme-soares <webdevguilhermes@gmail.com>
2026-06-01 20:46:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import * as gui_main from "./WEB-APP-PRO-GUI_MAIN/0_SETTINGS/gui_main.js";
-
-setTimeout(() => {
-    gui_main.func_open_new_window("WEB-APP-PRO-GUI_MAIN/1_LOADING/loading.html");
-}, 3000);
