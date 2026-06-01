ac80638c00db1ecbbcb862d57227a61b4ff792c1
web-dev-guilherme-soares <webdevguilhermes@gmail.com>
2026-06-01 09:03:34 -0300

    Delete WEB-APP-PRO-GUI_MAIN directory

--- WEB-APP-PRO-GUI_MAIN/3_PROFILE/profile.js
+++
@@ -1,7 +0,0 @@
-import * as gui_main from "../0_SETTINGS/gui_main.js";
-
-gui_main.func_add_onclick("m_img_close", function() { gui_main.func_back_window(); });
-
-gui_main.m_show_dialog(false, "VOID-777", "VOID-777 - Ops... parece que você\nencontrou o perfil do desenvolvedor 😯");
-
-gui_main.func_add_onclick("m_img_pf", function() { gui_main.m_show_img("../-1_IMAGES/m_my.png"); });
--- WEB-APP-PRO-GUI_MAIN/7_GAMES_PAGES/games_pages.js
+++
@@ -1,7 +0,0 @@
-import * as gui_main from "../0_SETTINGS/gui_main.js";
-
-gui_main.func_add_onclick("m_img_close", function() { gui_main.func_back_window(); });
-
-gui_main.m_show_dialog(false, "VOID-777", "VOID-777 — Esta página é divertida,\nnão é? Experimente algumas opções de jogos.");
-
-gui_main.func_add_onclick("m_btn_game_0", function() { gui_main.func_open_new_window("../GAMES/0_game/0_game.html"); });
